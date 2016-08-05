6666c0e37dab24e975e1c8dc378d1f44b5203161
Jaime Neves <jaimebarrosoneves@gmail.com>
2016-08-04 23:44:01 -0400

    firts commit

---
+++ checkforce.js
@@ -0,0 +1,15 @@
+(function(){
+
+	var input = document.querySelector("#check");
+	var uppercase = document.querySelector("#uppercase");
+	var lowercase = document.querySelector("#lowercase");
+
+	var characters = 8;
+
+	console.log(uppercase.lenght);
+	console.log(uppercase.lenght);
+	function check(element){
+
+	}
+
+})(window);
