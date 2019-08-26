58bd9bc7919f70d11ae5affc6109e1ed192b6924
Jaime Neves <jaimebarrosoneves@gmail.com>
2019-08-26 12:04:21 -0300

    best practices #42

--- src/core/password-length.js
+++ src/core/password-length.js
@@ -1,19 +1,19 @@
 /**
- * check length of the password
+ * Check length of the password
  */
 export const PasswordLength = (input, options = {}) => {
   const pwdlength = input.value.length
   let scores = 0
 
-    if (pwdlength > options.passIndex && pwdlength < options.minimumChars) {
-      // options.scores += 5
+    if ((pwdlength > options.passIndex) && (pwdlength < options.minimumChars)) {
       scores += 5
-    } else if ((pwdlength >= options.minimumChars) && (pwdlength <= options
-      .maximumChars)) {
-      // options.scores += 10
+    }
+    
+    if ((pwdlength >= options.minimumChars) && (pwdlength <= options.maximumChars)) {
       scores += 10
-    } else if (pwdlength > options.maximumChars) {
-      // options.scores += 25
+    }
+    
+    if (pwdlength > options.maximumChars) {
       scores += 25
     }
 
